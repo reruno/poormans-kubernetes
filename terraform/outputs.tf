edac4ef3683a6e5c7529e798e34f6ded2bcce14e
michael kan <michael.kan.mail@gmail.com>
2025-11-21 17:40:25 +0100

    add private ip for public instance in output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,11 @@ output "ec2_public_instance_1_public_ip" {
   value       = aws_instance.public_instance_1.public_ip
 }
 
+output "ec2_public_instance_1_private_ip" {
+  description = "The private IP address of the public_instance_1 instance."
+  value       = aws_instance.public_instance_1.private_ip
+}
+
 output "ec2_private_instance_1_private_ip" {
   description = "The private IP address of the private_instance_1 instance."
   value       = aws_instance.private_instance_1.private_ip
